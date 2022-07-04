a6ef73bc9c4b3be5c532dc48cf60aaa6af42607a
Max Ahmad <ahmadmax046@gmail.com>
2022-07-04 13:36:59 +0000

    attempted challenge 1 and 3

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+    for(i = 0; i < arrayOfNumbers.length; i++)
+        sum += arrayOfNumbers[i]
+        //console.log(sum)
+        return sum
+    //return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,12 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  // for(evenNum = 0; evenNum < arrayOfNumbers.length; evenNum+=2)
+  // console.log(evenNum)
+    //return evenNum
+    //return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ // assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +55,15 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let x = 9/5;
+  let convertedArray = []
+    for(i = 0; i < arrayOfNumbers.length; i++)
+      convertedArray.push(Math.floor((arrayOfNumbers[i] * x)) + 32)
+    //console.log(convertedArray)
+    return convertedArray
+   // return arrayOfNumbers;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
